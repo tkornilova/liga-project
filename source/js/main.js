c46bcee42ddd667dd2904d7313c549b61856cf1d
tkornilova <kornilova.tat07@gmail.com>
2023-11-25 22:33:18 +0400

    init: добавляет header

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@ import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {CustomSelect} from './modules/select/custom-select';
 import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
+import {initHeader} from './modules/header/init-header.js';
 
 // ---------------------------------
 
@@ -12,6 +13,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
 
   mobileVhFix();
+  initHeader();
 
   // Modules
   // ---------------------------------
